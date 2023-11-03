1513cc1708ca213180bcc5acbd0d12f63f814313
Taylan Utku ÖZEN <taylanutku.ozen@gmail.com>
2023-11-03 20:32:10 +0300

    Project EntityFramework Intro

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Business.Concrete;
-using DataAccess.Concrete.InMemory;
-
+using DataAccess.Concrete.InMemory; 
 ProductManager productManager = new ProductManager(new InMemoryProductDal());
 foreach (var product in productManager.GetAll())    
 {
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface ICategoryDal :IEntityRepository<Category>
     {
 
     }
