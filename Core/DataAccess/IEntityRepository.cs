2d0cd3982fc467e3aef6e457f0a6abec15b00565
Taylan Utku ÖZEN <taylanutku.ozen@gmail.com>
2023-11-06 20:06:13 +0300

    Kurumsal Mimari I Initializitilon

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,11 @@
-﻿using Entities.Abstract;
-using Entities.Concrete;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constraint
     //class : reference type olabilir.
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity implement eden class bir veritabanı tablosudur.
     public interface IEntity
